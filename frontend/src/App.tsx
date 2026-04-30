64369b2cc1d6fd16542e16a96a5c2cac559da5a2
ShahJabir <shahjabirofficial@gmail.com>
2026-04-30 21:42:06 +0600

    init: frontend project init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Thumbnail Builder</h1>
+      </div>
+      <label htmlFor="file-input">Choose an image:</label>
+      <input type="file" id="file-input" />
+    </>
+  );
+}
+
+export default App;
